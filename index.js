4e5427f4867bea47ad6fc2ae9cb0b2d8656f0d84
Chris Franklin <cf73021@gmail.com>
2019-03-01 02:05:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function Point(x, y){
   this.x = x;
   this.y = y;
 }
-
+Point.prototype.toString = function() {return
+`(${this.x},${this.y})`;}
 function Shape(x, y){
   this.x = x;
   this.y = y;
