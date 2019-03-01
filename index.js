9585b040fec025cb406917d37f1cffae28bf3a64
Chris Franklin <cf73021@gmail.com>
2019-03-01 02:11:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,15 @@ function Circle(radius){
   this.radius = radius
   this.diameter = function() {return this.radius * 2 ;}
   this.area = function() {return this.radius**2 * Math.PI ;}
-  this.circumference = function() {return this.radius * 2 * Math.PI ;} 
+  this.circumference = function() {return this.radius * 2 * Math.PI ;}
+}
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Shape;
+
+function Side(length){
+  this.length = length ;
 }
 
-function Polygon(){}
-function Side(){}
+function Polygon(sides) {
+  this.sides = sides ;
+}
